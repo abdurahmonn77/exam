39a4cdf0e69c34b5d7974a877ea0fa7bfee07fb2
Abdurahmon <abdurahmonikromov634@gmail.com>
2025-03-29 11:05:33 +0500

    exam

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,19 +3,22 @@ import Category from "@/modules/Category";
 import Hero from "@/modules/Hero";
 import Products from "@/modules/Products";
 import DiscountProduct from "@/modules/Products/DiscountProducts";
+import { Footer } from "antd/es/layout/layout";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("Products")
+  const t = useTranslations("Products");
   return (
     <>
-        <Hero/>
-        <Brands/>
-        <Products title={t("title")}/>
-        <Products title={t("title")} extraClass="hidden md:block"/>
-        <Products title={t("title")} extraClass="hidden md:block"/>
-        <Category/>
-        <DiscountProduct title={t("discount")}/>
+      <Hero />
+      <Brands />
+      <Products title={t("title")} />
+      <Products title={t("title")} extraClass="hidden md:block" />
+      <Products title={t("title")} extraClass="hidden md:block" />
+      <Category />
+      <DiscountProduct title={t("discount")} />
+      <DiscountProduct title={"Oxirgi Ko'rilgan maxsulotlar"} />
+      <Footer />
     </>
   );
 }
